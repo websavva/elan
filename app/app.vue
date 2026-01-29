45a47c7f020254fab011a12e6fd3eb1c12ab3b27
WebSavva <websavvap@gmail.com>
2026-01-30 01:16:59 +0300

    fix: Favicon url was fixed

--- app/app.vue
+++ app/app.vue
@@ -7,17 +7,17 @@
     <SectionDelimiter />
 
     <About />
-    
+
     <SectionDelimiter />
-    
+
     <Interior />
-    
+
     <SectionDelimiter />
-    
+
     <Indications />
-    
+
     <SectionDelimiter />
-    
+
     <Contacts />
   </div>
 </template>
@@ -30,33 +30,61 @@ const seoDescription = computed(() => t('seo.description'));
 const seoKeywords = computed(() => t('seo.keywords'));
 const seoSiteName = computed(() => t('seo.siteName'));
 const seoImageAlt = computed(() => t('seo.imageAlt'));
-const seoImage = computed(() => `${process.env.BASE_URL}/og.png`);
-const ogLocale = computed(() => (locale.value === 'fr' ? 'fr_CH' : 'en_CH'));
+const seoImage = computed(
+  () => `${process.env.BASE_URL}/og.png`,
+);
+const ogLocale = computed(() =>
+  locale.value === 'fr' ? 'fr_CH' : 'en_CH',
+);
 
 useHead({
   title: seoTitle,
   htmlAttrs: {
-    class: 'max-3xl:lg:text-[13px]'
+    class: 'max-3xl:lg:text-[13px]',
   },
   meta: [
     { name: 'description', content: seoDescription.value },
     { name: 'keywords', content: seoKeywords.value },
     // Open Graph
     { property: 'og:type', content: 'website' },
-    { property: 'og:site_name', content: seoSiteName.value },
+    {
+      property: 'og:site_name',
+      content: seoSiteName.value,
+    },
     { property: 'og:title', content: seoTitle.value },
-    { property: 'og:description', content: seoDescription.value },
+    {
+      property: 'og:description',
+      content: seoDescription.value,
+    },
     { property: 'og:image', content: seoImage.value },
-    { property: 'og:image:alt', content: seoImageAlt.value },
+    {
+      property: 'og:image:alt',
+      content: seoImageAlt.value,
+    },
     { property: 'og:url', content: process.env.BASE_URL },
     { property: 'og:locale', content: ogLocale.value },
     // Twitter
-    { name: 'twitter:card', content: 'summary_large_image' },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image',
+    },
     { name: 'twitter:title', content: seoTitle.value },
-    { name: 'twitter:description', content: seoDescription.value },
+    {
+      name: 'twitter:description',
+      content: seoDescription.value,
+    },
     { name: 'twitter:image', content: seoImage.value },
-    { name: 'twitter:image:alt', content: seoImageAlt.value },
+    {
+      name: 'twitter:image:alt',
+      content: seoImageAlt.value,
+    },
   ],
-  link: [{ rel: 'canonical', href: process.env.BASE_URL }],
+  link: [
+    { rel: 'canonical', href: process.env.BASE_URL },
+    {
+      href: process.env.BASE_URL + '/favicon.ico',
+      rel: 'icon',
+    },
+  ],
 });
 </script>
